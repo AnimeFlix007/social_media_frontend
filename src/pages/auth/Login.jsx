777909e8914e99f9cf12d3085ed6f7c0d847c6a6
AnimeFlix007 <devjitbose111@gmail.com>
2023-04-17 09:04:33 +0530

    login pg. formik validation

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useFormik } from "formik";
+import * as Yup from "Yup";
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>login</div>
-  )
-}
+  const { handleChange, handleBlur, errors, values, touched, handleSubmit } =
+    useFormik({
+      enableReinitialize: true,
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: Yup.object({
+        email: Yup.string()
+          .email("Enter a valid email")
+          .required("Email is Required"),
+        password: Yup.string()
+          .min(6, "Password should be more than 6 characters")
+          .required("Password is required"),
+      }),
+      onSubmit: (values, action) => {
+        console.log(values);
+        action.resetForm();
+      },
+    });
+  return <div>login</div>;
+};
 
-export default Login
+export default Login;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import PageNotFound from "../components/PageNotFound";
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path={"*"} element={<PageNotFound />} />
