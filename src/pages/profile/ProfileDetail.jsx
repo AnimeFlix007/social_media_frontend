414044f31e4acef6dbcc9e37097941bdb67669a4
AnimeFlix007 <devjitbose111@gmail.com>
2023-04-22 18:32:21 +0530

    handled for shady route

--- src/pages/profile/ProfileDetail.jsx
+++ src/pages/profile/ProfileDetail.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { userProfile } from "../../context/slice/userSlice";
+import BadRequestEmpty from "../../assets/BadRequestEmpty.avif";
+import "../../styles/profile/error-profile.css";
 
 const ProfileDetail = () => {
   const { id } = useParams();
@@ -17,7 +19,17 @@ const ProfileDetail = () => {
   return (
     <div className="main">
       {loading && <p>loading...</p>}
-      {!loading && invalid_user_profile && <p>Invalid user profile</p>}
+      {!loading && invalid_user_profile && (
+        <div className="bad-request-empty">
+          <h1>404: The Profile you are looking for isn’t here</h1>
+          <p>
+            You either tried some shady route or you came here by mistake.
+            Whichever it is, try using the navigation
+          </p>
+          <img src={BadRequestEmpty} alt="BadRequestEmpty" />
+          <button>View Your Profile</button>
+        </div>
+      )}
       {!loading && user_profile && <p>{user_profile?.username}</p>}
     </div>
   );
